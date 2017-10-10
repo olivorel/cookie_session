d5783fc1ec4410e03bc5a7c3b9b227282d44fb9b
olivorel <olivorel@users.noreply.github.com>
2017-10-10 08:36:45 +0200

    update cart.php

--- cart.php
+++ cart.php
@@ -43,16 +43,16 @@ if (empty($_SESSION['loginname'])) {
            <?php
             if ($addarticlecart == true) {
 
-                if ($_COOKIE['cookie1']) {
+                if (!empty($_COOKIE['cookie1'])) {
                 setcookie("cookie1", "", time() - 1);
                 }
-                if ($_COOKIE['cookie2']) {
+                if (!empty($_COOKIE['cookie2'])) {
                 setcookie("cookie2", "", time() - 1);
                 }
-                if ($_COOKIE['cookie3']) {
+                if (!empty($_COOKIE['cookie3'])) {
                 setcookie("cookie3", "", time() - 1);
                 }
-                if ($_COOKIE['cookie4']) {
+                if (!empty($_COOKIE['cookie4'])) {
                 setcookie("cookie4", "", time() - 1);
                 }
             ?>
